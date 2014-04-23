9148c151b0ba3884523770a8f889b5b946b8f7de
Anton Bondar <anton@zebooka.com>
2014-04-23 17:53:53 +0700

    Implemented test for Executor.

---
+++ tests/integration/Zebooka/PD/ExecutorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Zebooka\PD;
+
+class ExecutorTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_execute_ok()
+    {
+        $cmd = 'echo 123 >/dev/null 2>&1';
+        $executor = new Executor();
+        $code = $executor->execute($cmd);
+        $this->assertEquals(0, $code);
+    }
+
+    public function test_execute_failure()
+    {
+        $cmd = 'not-existing-command >/dev/null 2>&1';
+        $executor = new Executor();
+        $code = $executor->execute($cmd);
+        $this->assertEquals(127, $code);
+    }
+}
