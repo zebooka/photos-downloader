f2305638a6bf0deb8a6ee9cdd350aa4741edfa15
Anton Bondar <anton@zebooka.com>
2015-08-28 11:13:24 +0600

    Added test for Tokenizer and c1/vsco c1 tokens (there was some issue, that I can not reproduce when I got incorrect order c1_vsco in file name.

---
+++ tests/integration/Zebooka/PD/TokenizerIntegrationTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Zebooka\PD;
+
+class TokenizerIntegrationTest extends \PHPUnit_Framework_TestCase
+{
+    private function realConfigure()
+    {
+        return new \Zebooka\PD\Configure(
+            array(),
+            json_decode(file_get_contents(__DIR__ . '/../../../../res/tokens.json'), true)
+        );
+    }
+
+    /**
+     * @return ExifAnalyzer
+     */
+    private function exifAnalyzer(FileBunch $fileBunch, $datetime, $camera, $tokens = array())
+    {
+        return \Mockery::mock('\\Zebooka\\PD\\ExifAnalyzer')
+            ->shouldReceive('extractDateTimeCameraTokens')
+            ->with($fileBunch)
+            ->once()
+            ->andReturn(array($datetime, $camera, $tokens))
+            ->getMock();
+    }
+
+    /**
+     * @return FileBunch
+     */
+    private function fileBunch($basename)
+    {
+        return \Mockery::mock('\\Zebooka\\PD\\FileBunch')
+            ->shouldReceive('basename')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($basename)
+            ->getMock();
+    }
+
+    public function test_vsco_c1()
+    {
+        $configure = $this->realConfigure();
+        $configure->tokensDropUnknown = true;
+
+        // vsco c1 from basename
+        $fileBunch = $this->fileBunch('2015_1_test_vsco_c1');
+        $tokenizer = new Tokenizer(
+            $configure,
+            $this->exifAnalyzer($fileBunch, null, null, array())
+        );
+        $tokens = $tokenizer->tokenize($fileBunch);
+        $this->assertInstanceOf('\\Zebooka\\PD\\Tokens', $tokens);
+        $this->assertEquals(array('vsco', 'c1'), $tokens->tokens);
+
+        // c1 in exif and vcso c1 in basename
+        $fileBunch = $this->fileBunch('2015_2_test_vsco_c1');
+        $tokenizer = new Tokenizer(
+            $configure,
+            $this->exifAnalyzer($fileBunch, null, null, array('c1'))
+        );
+        $tokens = $tokenizer->tokenize($fileBunch);
+        $this->assertInstanceOf('\\Zebooka\\PD\\Tokens', $tokens);
+        $this->assertEquals(array('vsco', 'c1'), $tokens->tokens);
+
+        // vsco c1 in exif and c1 in basename
+        $fileBunch = $this->fileBunch('2015_3_test_c1');
+        $tokenizer = new Tokenizer(
+            $configure,
+            $this->exifAnalyzer($fileBunch, null, null, array('vsco', 'c1'))
+        );
+        $tokens = $tokenizer->tokenize($fileBunch);
+        $this->assertInstanceOf('\\Zebooka\\PD\\Tokens', $tokens);
+        $this->assertEquals(array('c1', 'vsco'), $tokens->tokens);
+
+        // vsco c1 in exif and vcso c1 in basename
+        $fileBunch = $this->fileBunch('2015_4_test_vsco_c1');
+        $tokenizer = new Tokenizer(
+            $configure,
+            $this->exifAnalyzer($fileBunch, null, null, array('vsco', 'c1'))
+        );
+        $tokens = $tokenizer->tokenize($fileBunch);
+        $this->assertInstanceOf('\\Zebooka\\PD\\Tokens', $tokens);
+        $this->assertEquals(array('vsco', 'c1'), $tokens->tokens);
+    }
+}
